60f82f377ccfef5f6f9f7c154d84b85c20c13f10
ideateGudy <azonubigoodnews@gmail.com>
2025-03-26 15:30:05 +0100

    starter file setup

---
+++ controllers/authController.js
@@ -0,0 +1,9 @@
+const registerUser = async (req, res) => {
+  res.send("Registration successful");
+};
+
+const loginUser = async (req, res) => {
+  res.send("Login successful");
+};
+
+export { registerUser, loginUser };
